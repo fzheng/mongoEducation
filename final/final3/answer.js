8f1fef10724c76bd9c13542296de677cfa9a407e
fzheng <feng.zheng@freecause.com>
2013-12-05 16:29:42 -0500

    continue final

---
+++ final/final3/answer.js
@@ -0,0 +1,10 @@
+db.messages.update(
+  {
+    'headers.Message-ID': '<8147308.1075851042335.JavaMail.evans@thyme>'
+  },
+  {
+    $push: {
+      'headers.To': 'mrpotatohead@mongodb.com'
+    }
+  }
+);
